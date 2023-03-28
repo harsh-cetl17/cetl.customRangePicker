a59fa79d8737c49c0aaef043a90884cd8b200f6d
harsh-cetl17 <129154210+harsh-cetl17@users.noreply.github.com>
2023-03-28 12:59:26 +0530

    Add files via upload

---
+++ build/generated/source/buildConfig/debug/com/savvi/rangedatepicker/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.savvi.rangedatepicker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.savvi.rangedatepicker";
+  public static final String BUILD_TYPE = "debug";
+}
